99fe1d5e2be09222a6e13aa0663ca4402cef5b26
elizabethbright721 <elizabethbright721@gmail.com>
2022-12-15 14:03:42 -0500

    Events Lab

--- main.js
+++ main.js
@@ -7,3 +7,34 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+//Create variable that will contain each div with the class of color
+const allColors = document.querySelectorAll("#palette div.color");
+
+//Create variable that will contain the current color
+const currentColor = document.querySelector("#current-color");
+
+//loop through 'color' variable and add an event listener for each color that will change the background of the 'current-color' element to the clicked color
+
+allColors.forEach((selectedColor) => {
+  selectedColor.addEventListener("click", () => {
+  currentColor.style.background = selectedColor.style.background; 
+  });
+});
+
+//Create a variable that will contain each dive with the class of cell
+const allCells = document.querySelectorAll(".cell")
+
+//loop through 'gridColor' variable and add an event listener for each grid cell when each cell is click to change the color to the 'selectedColor' element
+
+allCells.forEach((cell) => {
+  cell.addEventListener("click" , (event) => {
+  event.target.style.background = currentColor.style.background;
+  })
+  cell.addEventListener('dblclick', erase);
+})
+
+function erase(event) {
+  event.target.style.background = "white"
+}
+
+
